334cd61143e37f00e79bb310335deff934df0ca8
Ansore <aansore@outlook.com>
2022-05-23 01:16:25 +0800

    ipam test

--- network/ipam_test.go
+++ network/ipam_test.go
@@ -8,12 +8,28 @@ import (
 func TestAllocate(t *testing.T) {
 	_, ipnet, _ := net.ParseCIDR("192.168.0.1/24")
 	ip, _ := ipAllocator.Allocate(ipnet)
-	t.Logf("alloc ip: %v", ip)
+	t.Logf("alloc ip1: %v\n", ip)
+
+	_, ipnet, _ = net.ParseCIDR("192.168.0.2/24")
+	ip, _ = ipAllocator.Allocate(ipnet)
+	t.Logf("alloc ip2: %v\n", ip)
+
+	_, ipnet, _ = net.ParseCIDR("192.168.0.3/24")
+	ip, _ = ipAllocator.Allocate(ipnet)
+	t.Logf("alloc ip2: %v\n", ip)
 }
 
 func TestRelease(t *testing.T) {
 	ip, ipnet, _ := net.ParseCIDR("192.168.0.1/24")
-  if err := ipAllocator.Release(ipnet, &ip); err != nil {
-    t.Logf("error %v", err)
-  }
+	if err := ipAllocator.Release(ipnet, &ip); err != nil {
+		t.Logf("error %v\n", err)
+	}
+	ip, ipnet, _ = net.ParseCIDR("192.168.0.2/24")
+	if err := ipAllocator.Release(ipnet, &ip); err != nil {
+		t.Logf("error %v\n", err)
+	}
+	ip, ipnet, _ = net.ParseCIDR("192.168.0.3/24")
+	if err := ipAllocator.Release(ipnet, &ip); err != nil {
+		t.Logf("error %v\n", err)
+	}
 }
